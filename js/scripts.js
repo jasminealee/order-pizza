3b3fa45ad89751346486d010dcd813c959948b68
Jasmine Lee <jasmine.al1722@gmail.com>
2019-02-01 13:26:25 -0800

    add more to else if statments and centered closing else statement

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,15 @@ function Pizza() {
 }
 
 Pizza.prototype.newPizzaId = function() {
-  this.pizzaId=+;
+  this.pizzaId++;
 }
+
+Pizza.prototype.newPizza = function() {
+    if (this.toppings.length >= 0) {
+        this.toppingsId = 0;
+        this.pizzaSize = [];
+        this.total = 0;
+    };
 
 Pizza.prototype.addSize = function(size) {
   this.pizzaSize.push(size)
@@ -35,6 +42,9 @@ Pizza.prototype.sizeCostToppings = function() {
         this.pizzaSize[0].cost = 7.00;
     } else if (this.pizzaSize[0].size === "Large") {
         this.pizzaSize[0].cost = 12.00;
+    } else {
+      return "I'm sorry. I'm not able to finalize your order."
+    }
     };
 }
 
